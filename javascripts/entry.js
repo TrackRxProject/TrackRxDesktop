b7b6e77628413fb82bfe469717cb24d040147153
Everett Berry <epberry@purdue.edu>
2016-02-08 12:28:20 -0500

    working on network requests - also updated react style to match mobile app

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -2,32 +2,55 @@
 
 var React = require('react');
 var ReactDom = require('react-dom');
+var Component = React.Component;
 
-var CommentBox = React.createClass({
-  render: function() {
+class PatientBox extends Component {
+  buttonClicked() {
+    console.log('button clicked');
+    var request = new Request('http://trackrx.xyz:8000/prescription/1', {
+      headers: new Headers({
+        'Content-Type': 'text/plain'
+      })
+    });
+    fetch(request, {
+      method: 'get',
+      mode: 'no-cors'
+    }).then(function(response) {
+      console.log(response);
+      return response.text(); 
+    }).then(function(text) {
+      console.log(text);
+    }).catch(function(err) {
+      console.log('error');
+      console.log(err);
+    });
+  }
+
+  render() {
     console.log('render');
     return (
-      <div className="commentBox">
-        Patient List
+      <div>
+        <p>Patient 1</p>
+        <button onClick={this.buttonClicked.bind(this)}>Refresh Bottles</button>
       </div>
     );
   }
-});
+}
 
-var App = React.createClass({
-  render: function() {
-  return (
-    <div>
-      <div style={navStyle}>
-        <h1 style={navStyle}>TrackRx Pharmacy Application</h1>
-      </div>
+class App extends Component {
+  render() {
+    return (
       <div>
-        <CommentBox />
+        <div style={navStyle}>
+          <h1 style={navStyle}>TrackRx Pharmacy Application</h1>
+        </div>
+        <div>
+          <PatientBox />
+        </div>
       </div>
-    </div>
-  );
+    );
   }
-});
+}
 
 var navStyle = {
   flex: 1,
