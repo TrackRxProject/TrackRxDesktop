b7386b0b1d9ef542dd8865044ead2d400833bbaa
Everett Berry <epberry@purdue.edu>
2016-02-15 12:19:18 -0500

    doing interactivity the react way

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -5,13 +5,22 @@ var ReactDom = require('react-dom');
 var Component = React.Component;
 
 class PatientBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: 'No bottles'};
+  }
+
   buttonClicked() {
+    console.log(this);
     console.log('button clicked');
-    var request = new Request('http://trackrx.xyz:8000/prescription/1', {
+      var request = new Request('http://trackrx.xyz:8000/prescription/1/activate', {
       headers: new Headers({
-        'Content-Type': 'text/plain'
+      'Content-Type': 'application/json'
       })
     });
+
+    var that = this; // save for updating state inside fetch
+
     fetch(request, {
       method: 'GET',
       mode: 'cors'
@@ -19,14 +28,18 @@ class PatientBox extends Component {
       return response.text()
     }).then(function(text) {
       console.log(text);
+      var data = JSON.parse(text);
+      console.log(document.getElementById("patient"));
+      if (data.activate) {
+        that.setState({data: data.interval});
+      }
     });
   }
 
   render() {
-    console.log('render');
     return (
       <div>
-        <p>Patient 1</p>
+        <p id="patient">{this.state.data}</p>
         <button onClick={this.buttonClicked.bind(this)}>Refresh Bottles</button>
       </div>
     );
