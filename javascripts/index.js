03eecffa38fa7f0b772b882c4b5744c46fb9ac11
Everett Berry <epberry@purdue.edu>
2016-04-01 13:25:52 -0400

    Fix react-router for electron

--- javascripts/index.js
+++ javascripts/index.js
@@ -4,27 +4,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import mui from 'material-ui';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import {createHashHistory} from 'history';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './App';
 import Home from './Home';
 
 injectTapEventPlugin();
 
-/*
-const routes = (
-    <Route path="/" handler={App}>
-    <DefaultRoute handler={App} />
-    <Route name="about" handler={App} />
-    <Route name="contact" handler={App} />
-    </Route>
-    );
-    */
-
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path='/' component={App}>
       <IndexRoute component={Home}/>
     </Route>
   </Router>,
-  document.getElementById('content')) 
+  document.getElementById('content')
+);
+
