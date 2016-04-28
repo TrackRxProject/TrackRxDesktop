bfea76e60e860b092eb07b520f125beaa5306d21
Everett Berry <epberry@purdue.edu>
2016-04-28 03:15:28 -0400

    example chart displaying

--- javascripts/Patients.js
+++ javascripts/Patients.js
@@ -2,11 +2,34 @@
 
 import React from 'react'
 
+var LineChart = require('react-chartjs/lib/line');
+var Chart = require('chart.js')
+
 export default class Patients extends React.Component {
+  // kvar LineChart = require("react-chartjs").Line;
+   
   render() {
+    var chartData = {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3]
+      }]
+    };
+
+    var chartOptions =  {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    };
+
     return (
       <div>
-        patients
+        <LineChart data={chartData} options={chartOptions} width="600" height="250"/>
       </div>
     );
   }
